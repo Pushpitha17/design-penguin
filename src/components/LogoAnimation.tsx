243c4881881de8229c6ed7a6e88d5d4bb3f89029
Pushpitha <pushpithagee@gmail.com>
2024-06-27 16:37:44 +0530

    fix : Build error

--- src/components/LogoAnimation.tsx
+++ src/components/LogoAnimation.tsx
@@ -22,9 +22,9 @@ function LogoAnimation() {
   return (
     <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
       <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll-left">
-        {logos.map((item) => (
+        {logos.map((item, index) => (
           <li>
-            <img src={path + item} className="size-20" />
+            <img key={index} src={path + item} className="size-20" />
           </li>
         ))}
       </ul>
@@ -32,9 +32,9 @@ function LogoAnimation() {
         className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll-left"
         aria-hidden="true"
       >
-        {logos.map((item) => (
+        {logos.map((item, index) => (
           <li>
-            <img src={path + item} className="size-20" />
+            <img  key={index} src={path + item} className="size-20" />
           </li>
         ))}
       </ul>
